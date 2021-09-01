17b6c759290c9eb2a0312dcede4cc9d5a9ef73f5
Eva <yi.zheng1709@gmail.com>
2021-09-01 08:42:54 -0500

    adds more objects to array; more pics

--- src/components/Merch/Merch.js
+++ src/components/Merch/Merch.js
@@ -7,7 +7,17 @@ import matcha from './images/matcha.jpg'
 import ItemCard from '../ItemCard/ItemCard'
 import tank from './images/tank.jpg'
 import apron from './images/apron.jpg'
-import legging from './images/legging.jpg'
+import dress from './images/dress.jpg'
+import shower from './images/shower.jpg'
+import book from './images/book.jpg'
+import muscles from './images/muscles.jpg'
+import phone from './images/phone.jpg'
+import pin from './images/pin.jpg'
+import mat from './images/mat.jpg'
+import note from './images/note.jpg'
+import roll from './images/roll.jpg'
+import tote from './images/tote.jpg'
+import boba from './images/boba.jpg'
 
 export default function Merch() 
 {
@@ -43,9 +53,60 @@ export default function Merch()
             link: 'https://www.redbubble.com/i/apron/I-love-you-so-matcha-by-GirlyProgrammer/86543201.6ZXWR?asc=u'
         },
         {
-            name: 'Spring is Here Leggings',
-            img: legging,
-            link: 'https://girly-programmer.creator-spring.com/listing/spring-is-here-august-2021?product=641&variation=102636&size=4161'
+            name: 'Obsessed with Sakura A-Line Dress',
+            img: dress,
+            link: 'https://www.redbubble.com/i/dress/Obsessed-with-Sakura-by-GirlyProgrammer/87312000.V4WQ8?asc=u'
+        },
+        {
+            name: 'Obsessed with Sakura Shower Curtain',
+            img: shower, 
+            link: 'https://www.redbubble.com/i/shower-curtain/Obsessed-with-Sakura-by-GirlyProgrammer/87312000.YH6LW?asc=u'
+        },
+        {
+            name: 'Obsessed with Sakura Hardcover Journal',
+            img: book, 
+            link: 'https://www.redbubble.com/i/notebook/Obsessed-with-Sakura-by-GirlyProgrammer/87312000.RXH2R?asc=u'
+        },
+        {
+            name: 'this took discipline and dedication Tank Top',
+            img: muscles,
+            link: 'https://www.redbubble.com/i/tank-top/this-took-discipline-and-dedication-by-GirlyProgrammer/86887593.Z81UR?asc=u'
+        },
+        {
+            name: 'this took discipline and dedication iPhone Case & Cover',
+            img: phone,
+            link: 'https://www.redbubble.com/i/iphone-case/this-took-discipline-and-dedication-by-GirlyProgrammer/86887593.23I0V?asc=u',
+
+        },
+        {
+            name: 'this took discipline and dedication Pin',
+            img: pin,
+            link: 'https://www.redbubble.com/i/pin/this-took-discipline-and-dedication-by-GirlyProgrammer/86887593.NP9QY?asc=u'
+        },
+        {
+            name: 'I am <human /> Mouse Pad',
+            img: mat,
+            link: 'https://www.redbubble.com/i/mouse-pad/I-am-human-by-GirlyProgrammer/86882232.GAP22?asc=u'
+        },
+        {
+            name: 'I am <human /> Spiral Notebook',
+            img: note,
+            link: 'https://www.redbubble.com/i/notebook/I-am-human-by-GirlyProgrammer/86882232.WX3NH?asc=u'
+        },
+        {
+            name: "that's how i roll Tri-blend T-Shirt",
+            img: roll, 
+            link: 'https://www.redbubble.com/i/t-shirt/that-s-how-i-roll-by-GirlyProgrammer/86520541.SWO0S?asc=u'
+        },
+        {
+            name: "that's how i roll Tote Bag",
+            img: tote,
+            link: 'https://www.redbubble.com/i/tote-bag/that-s-how-i-roll-by-GirlyProgrammer/86520541.P1QBH?asc=u'
+        },
+        {
+            name: 'best-teas forever! Cap',
+            img: boba,
+            link: 'https://www.redbubble.com/i/hat/best-teas-forever-by-GirlyProgrammer/86216093.XAK6W?asc=u'
         }
     
     ]
